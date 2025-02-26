ed59c61cd442cf81c35c15390fa1744ed636093d
Jack Clackett <jack@noquarter.co>
2025-02-26 10:56:37 +0100

    hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,14 @@ export default function Page() {
   return (
     <div className="w-screen h-dvh bg-orange-50/20">
       <div className="flex md:flex-row max-w-4xl flex-col mx-auto items-center justify-center h-dvh p-4 gap-4">
-        <Link href="https://mixer.j3c7.dev" className="text-2xl font-bold border" target="_blank">
+        <Link href="https://mixer.j3c7.dev" className="text-2xl font-bold border hover:mix-blend-difference" target="_blank">
           <Image src={mixer} alt="Mixer" className="h-[300px] object-cover" />
         </Link>
-        <Link href="https://rate-my-next.j3c7.dev" className="text-2xl font-bold border" target="_blank">
+        <Link
+          href="https://rate-my-next.j3c7.dev"
+          className="text-2xl font-bold border hover:mix-blend-difference"
+          target="_blank"
+        >
           <Image src={rateMyNext} alt="Rate My Next" className="h-[300px] object-cover" />
         </Link>
       </div>
