ac9cec8ca8b95e5ee02089e0e2162429a53e6453
Jack Clackett <jack@noquarter.co>
2025-03-14 17:44:48 +0100

    add new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,40 @@ import Image from "next/image"
 import Link from "next/link"
 import mixer from "../../public/mixer.png"
 import rateMyNext from "../../public/rate-my-next.png"
-
+import synth from "../../public/synth.png"
 export default function Page() {
   return (
     <div className="w-screen h-dvh bg-orange-50/20">
-      <div className="flex md:flex-row max-w-4xl flex-col mx-auto items-center justify-center h-dvh p-4 gap-4">
-        <Link href="https://mixer.j3c7.dev" className="text-2xl font-bold border hover:mix-blend-difference" target="_blank">
-          <Image src={mixer} alt="Mixer" className="h-[300px] object-cover" />
+      <div className="flex max-w-7xl mx-auto flex-wrap items-center h-full justify-center p-4 gap-4">
+        <Link
+          href="https://mixer.j3c7.dev"
+          className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
+          target="_blank"
+        >
+          <Image src={mixer} alt="Mixer" className="h-[300px] w-[400px] object-cover" />
+          <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
+            <p>Mixer</p>
+          </div>
+        </Link>
+        <Link
+          href="https://synth.j3c7.dev"
+          className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
+          target="_blank"
+        >
+          <Image src={synth} alt="Synth" className="h-[300px] w-[400px] object-cover" />
+          <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
+            <p>Synth</p>
+          </div>
         </Link>
         <Link
           href="https://rate-my-next.j3c7.dev"
-          className="text-2xl font-bold border hover:mix-blend-difference"
+          className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
           target="_blank"
         >
-          <Image src={rateMyNext} alt="Rate My Next" className="h-[300px] object-cover" />
+          <Image src={rateMyNext} alt="Rate My Next" className="h-[300px] w-[400px] object-cover" />
+          <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
+            <p>Rate My Next</p>
+          </div>
         </Link>
       </div>
     </div>
