c3bd867ca2e9bfe34226ed8efa9c5512b2461720
Jack Clackett <jack@noquarter.co>
2025-05-27 19:14:56 +0200

    tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,14 @@ import synth from "../../public/synth.png"
 
 export default function Page() {
   return (
-    <div className="w-screen h-dvh overflow-scroll bg-orange-50/20">
-      <div className="flex max-w-7xl mx-auto flex-wrap items-center h-full justify-center p-4 gap-4">
-        <Link
-          href="https://home.j3c7.dev"
-          className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
-          target="_blank"
-        >
-          <Image src={home} alt="Home" className="h-[220px] w-[300px] object-cover" />
-          <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
-            <p>Home</p>
-          </div>
-        </Link>
+    <div className="w-screen bg-background">
+      <div className="pt-24 flex max-w-7xl mx-auto flex-wrap items-center justify-center h-full p-4 gap-4">
         <Link
           href="https://mixer.j3c7.dev"
           className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
           target="_blank"
         >
-          <Image src={mixer} alt="Mixer" className="h-[220px] w-[300px] object-cover" />
+          <Image src={mixer} alt="Mixer" className="h-[220px] w-[350px] object-cover" />
           <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
             <p>Mixer</p>
           </div>
@@ -34,9 +24,19 @@ export default function Page() {
           className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
           target="_blank"
         >
-          <Image src={synth} alt="Synth" className="h-[220px] w-[300px] object-cover" />
+          <Image src={synth} alt="Synth" className="h-[220px] w-[350px] object-cover" />
           <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
             <p>Synth</p>
+          </div>
+        </Link>
+        <Link
+          href="https://home.j3c7.dev"
+          className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
+          target="_blank"
+        >
+          <Image src={home} alt="Home" className="h-[220px] w-[350px] object-cover" />
+          <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
+            <p>Home</p>
           </div>
         </Link>
         <Link
@@ -44,7 +44,7 @@ export default function Page() {
           className="group text-2xl flex relative font-bold border hover:mix-blend-difference"
           target="_blank"
         >
-          <Image src={nokia} alt="Nokia 3310" className="h-[220px] w-[300px] object-cover" />
+          <Image src={nokia} alt="Nokia 3310" className="h-[220px] w-[350px] object-cover" />
           <div className="absolute hidden group-hover:flex top-4 left-0 right-0 items-center justify-center">
             <p>Nokia 3310</p>
           </div>
